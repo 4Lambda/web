6d5aeff795b0c2d83c959e61a367c051498a3cef
Russell Bunch <rusty@4lambda.io>
2016-11-19 04:22:07 -0600

    Adding website with tox, travis, and Docker

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+
+@app.route('/', methods=['GET'])
+def landing():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='4l-web',
+    version='1.0.0',
+    url='http://www.4lambda.io',
+    license='Apache License 2.0',
+    author='Russell Bunch',
+    author_email='wm@4lambda.io',
+    description='Web. landing for 4Lambda.',
+    install_requires=[
+        'flask>0.10',
+        'flask_bootstrap>3.3.0.0'
+    ]
+)
