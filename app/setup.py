823172f016cb756a5e214c3c0be447215cb20ac9
Russell Bunch <rusty@4lambda.io>
2016-11-23 18:34:10 -0600

    Final commits for version 1.1.2

--- app/setup.py
+++ app/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='4l-web',
-    version='1.0.0',
+    version='1.1.2',
     url='http://www.4lambda.io',
     license='Apache License 2.0',
-    author='Russell Bunch',
-    author_email='wm@4lambda.io',
+    author='4Lambda Developers',
+    author_email='d@4lambda.io',
     description='Web. landing for 4Lambda.',
     install_requires=[
         'flask>0.10',
