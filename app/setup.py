1a0fa2a5e7e74b304aa352cac068a982d6d49c01
Russell Bunch <rusty@4lambda.io>
2016-12-30 15:57:06 -0600

    Removing redundant requirements and fixing neglected versioning

--- app/setup.py
+++ app/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='4l-web',
-    version='1.1.2',
+    version='1.2.8',
     url='http://www.4lambda.io',
     license='Apache License 2.0',
     author='4Lambda Developers',
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         'flask>0.10',
         'flask_bootstrap>3.3.0.0',
-        'uwsgi>2.2.0',
+        'uwsgi>2.0.0',
         'supervisor>3.3.0',
         'flask_cache>0.13.0',
         'flask_compress>1.3.0',
