b2cb8cfb7eebde7c54882fc3f86b9c3582fe2384
Russell Bunch <rusty@4lambda.io>
2017-05-30 19:25:19 -0500

    Repairing JQuery file.. lol

--- app/static/main.js
+++ app/static/main.js
@@ -1,28 +1,11 @@
-(function (global, factory) {
+$(document).ready(function () {
     "use strict";
-    if (typeof module === "object" && typeof module.exports === "object") {
-        if (global.document) {
-            module.exports = factory(global, true);
-        } else {
-            module.exports = function (w) {
-                if (!w.document) {
-                    throw new Error("jQuery requires a window with a document");
-                }
-                return factory(w);
-            };
-        }
-    } else {
-        factory(global);
-    }
-})(typeof window !== "undefined" ? window : function (window) {
     var scripts = ["facebook", "colors"];
     scripts.forEach(function (script) {
-        $.getScript(script + ".js")
-            .done(function (script, textStatus) {
-                console.log(textStatus);
-                })
-            .fail(function (jqxhr, settings, exception) {
-                $("div.log").text("Triggered ajaxError handler.");
+        $.getScript("./static/" + script + ".js").done(function (script, textStatus) {
+            console.log(textStatus);
+        }).fail(function (jqxhr, settings, exception) {
+            console.log(jqxhr, settings, exception);
         });
     });
 });
